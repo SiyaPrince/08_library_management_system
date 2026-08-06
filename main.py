1663f26cb61675042029b0dd3e843b5d8b3829e8
SiyaPrince <nkosiprince02@gmail.com>
2026-08-06 16:03:07 +0200

    helper functions to display a book, a member, and a loan

--- main.py
+++ main.py
@@ -1,2 +1,4 @@
+from run_library_system import run_library_system
+
 if __name__ == "__main__":
     run_library_system()
--- support_operations/displayers.py
+++ support_operations/displayers.py
@@ -1,8 +1,19 @@
 def display_book(book):
-    return
+    print(f"Book ID: {book['id']}")
+    print(f"Title: {book['title']}")
+    print(f"Author: {book['author']}")
+    print(f"Genre: {book['genre']}")
+    print(f"Status: {book['status']}")
 
 def display_member(member):
-    return
+    print(f"Member ID: {member['id']}")
+    print(f"Name: {member['name']}")
+    print(f"Email: {member['email']}")
+    print(f"Status: {member['status']}")
 
 def display_loan(loan, books, members):
-    return
+    print(f"Loan ID: {loan['id']}")
+    print(f"Book: {books[loan['book_id']]['title']}")
+    print(f"Member: {members[loan['member_id']]['name']}")
+    print(f"Due Date: {loan['due_date']}")
+    print(f"Status: {loan['status']}")
