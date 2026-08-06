f28a997250e299dbc26a1ae747ddaadac653497d
SiyaPrince <nkosiprince02@gmail.com>
2026-08-06 08:55:24 +0200

    these are the intial validators that will be used to validate the inputs

---
+++ support_operations/validators.py
@@ -0,0 +1,15 @@
+def validate_email(email):
+    return 
+
+def validate_phone_number(phone_number):
+    return
+
+def validate_date(date_string):
+    return
+
+def validate_book_title(title):
+    return
+
+def validate_member_name(name):
+    return
+
