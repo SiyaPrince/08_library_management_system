c2e18275e989540d2bc3b6c8d9046222f3cbd203
SiyaPrince <nkosiprince02@gmail.com>
2026-08-14 12:40:50 +0200

    updated members function

--- member_operations/view_members.py
+++ member_operations/view_members.py
@@ -1,6 +1,12 @@
-def view_members(members):
+from support_operations.displayers import display_member
 
+
+def view_members(members: list[dict]) -> None:
+    if not members:
+        print("No members found.")
+        return
+
+    print(f"\nmembers: ({len(members)})")
     print("=" * 45)
-    print("\nMembers: ")
-    print(members)
-    print("=" * 45)
+    for number, member in enumerate(members, start=1):
+        display_member(member, number)
