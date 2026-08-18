eb031980617d37676c6038c188b2b0a926eff898
SiyaPrince <nkosiprince02@gmail.com>
2026-08-18 22:15:24 +0200

    control functions reworked!

--- run_library_system.py
+++ run_library_system.py
@@ -6,49 +6,41 @@ from loan_operations.borrow_book import borrow_book
 from loan_operations.return_book import return_book
 from loan_operations.view_loans import view_active_loans
 from report_operations.display_summary import display_summary
-
 from member_operations.add_member import add_member
 from member_operations.view_members import view_members
 
-def run_library_system():
 
+def run_library_system():
     books = []
     members = []
     loans = []
 
-    # Display welcome message
     display_welcome_message()
 
     while True:
-            # Display menu
-    
-            display_menu()
-    
-            # Ask for choice
-            operation_choice = input("\nPlease choose operation: ").strip().lower()
-    
-            if operation_choice == "1":
-                add_book(books)
-            elif operation_choice == "2":    
-                search_books()
-            elif operation_choice == "3":
-                borrow_book(books)
-            elif operation_choice == "4":
-                return_book(books)
-            elif operation_choice == "5":
-                view_books()
-            elif operation_choice == "6":
-                add_member()
-            elif operation_choice == "7":
-                view_members()
-            elif operation_choice == "8":
-                view_books()
-            elif operation_choice == "9":
-                display_summary(books, members, loans)
-            elif operation_choice == "10":
-                # Exit
-                print("\nExiting the To-Do List Manager. Goodbye!")
-                break
-    
-            else:
-                print("\nInvalid choice. Please try again.")
+        display_menu()
+        operation_choice = input("\nPlease choose operation: ").strip()
+
+        if operation_choice == "1":
+            add_book(books)
+        elif operation_choice == "2":
+            search_books(books)
+        elif operation_choice == "3":
+            borrow_book(books, members, loans)
+        elif operation_choice == "4":
+            return_book(books, members, loans)
+        elif operation_choice == "5":
+            view_books(books)
+        elif operation_choice == "6":
+            add_member(members)
+        elif operation_choice == "7":
+            view_members(members)
+        elif operation_choice == "8":
+            view_active_loans(loans, books, members)
+        elif operation_choice == "9":
+            display_summary(books, members, loans)
+        elif operation_choice == "10":
+            print("\nExiting the Library Management System. Goodbye!")
+            break
+        else:
+            print("\nInvalid choice. Please try again.")
